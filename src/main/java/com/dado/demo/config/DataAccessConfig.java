3f9ce78fedbeb58dc377e9022d137e68a8cfccfa
alex-jo <whrhkddms5@gmail.com>
2021-01-06 16:26:09 +0900

    먼데

--- src/main/java/com/dado/demo/config/DataAccessConfig.java
+++ src/main/java/com/dado/demo/config/DataAccessConfig.java
@@ -2,7 +2,6 @@ package com.dado.demo.config;
 
 import javax.sql.DataSource;
 
-import org.apache.catalina.core.ApplicationContext;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.SqlSessionTemplate;
