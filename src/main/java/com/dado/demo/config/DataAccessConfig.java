56c2e20ef69d40a4349a8bed6d02217a87adfa0c
alex-jo <whrhkddms5@gmail.com>
2021-01-06 16:14:36 +0900

    왜 다 원복이 됐지

--- src/main/java/com/dado/demo/config/DataAccessConfig.java
+++ src/main/java/com/dado/demo/config/DataAccessConfig.java
@@ -7,10 +7,10 @@ import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 //import org.mybatis.spring.annotation.MapperScan;
@@ -22,46 +22,24 @@ import com.zaxxer.hikari.HikariDataSource;
 import lombok.AllArgsConstructor;
 
 @Configuration
-@EnableAspectJAutoProxy
 @EnableTransactionManagement
 @AllArgsConstructor
 @MapperScan(basePackages = "com.dado.demo.mapper")
-public class DataAccessConfig {
-
-	private ApplicationContext applicationContext;
+@PropertySource("classpath:/application.properties")
+@ConfigurationProperties(prefix = "spring.datasource.hikari")
+public class DataAccessConfig extends HikariConfig {
 	
 	@Bean
-    public DataSource dataSource() {
-        HikariConfig hikariConfig = new HikariConfig();
-        hikariConfig.setDriverClassName("${db.driverName}");
-        hikariConfig.setJdbcUrl("jdbc:log4jdbc:oracle:thin:@172.20.30.208:1521:hywel");
-
-        hikariConfig.setUsername("dado");
-        hikariConfig.setPassword("123456");
-
-        hikariConfig.setMinimumIdle(5);
-        // test Query
-        hikariConfig.setConnectionTestQuery("SELECT sysdate FROM dual");
-        hikariConfig.setPoolName("springHikariCP");
-
-        hikariConfig.addDataSourceProperty("dataSource.cachePrepStmts", "true");
-        hikariConfig.addDataSourceProperty("dataSource.prepStmtCacheSize", "200");
-        hikariConfig.addDataSourceProperty("dataSource.prepStmtCacheSqlLimit", "2048");
-        hikariConfig.addDataSourceProperty("dataSource.useServerPrepStmts", "true");
-
-        HikariDataSource dataSource = new HikariDataSource(hikariConfig);
-
-        return dataSource;
+    public DataSource dataSource() {		
+        return new HikariDataSource(this);
     }
 	
 	@Bean
 	public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
-		
 		SqlSessionFactoryBean sqlSessionFactory = new SqlSessionFactoryBean();
-        sqlSessionFactory.setDataSource(dataSource());
+        sqlSessionFactory.setDataSource(dataSource);
         sqlSessionFactory.setMapperLocations(
 				new PathMatchingResourcePatternResolver().getResources("classpath:mappers/*.xml"));
-        
 		return sqlSessionFactory.getObject();
 	}
 	
